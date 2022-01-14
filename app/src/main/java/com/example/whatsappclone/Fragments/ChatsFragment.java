0b12e2b92de7ac828a6bbd1c435daaf3977bfaad
MuhammadBilal <muhammadbilal20130@gmail.com>
2022-01-13 22:37:16 -0800

    fav icon added in chats

--- app/src/main/java/com/example/whatsappclone/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/whatsappclone/Fragments/ChatsFragment.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.whatsappclone.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ChatsFragment extends Fragment {
+    FloatingActionButton fab;
 
 
     public ChatsFragment() {
@@ -21,7 +24,13 @@ public class ChatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_chats, container, false);
-
+        fab=view.findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"New Chats",Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 }
